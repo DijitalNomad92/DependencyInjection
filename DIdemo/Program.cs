d4fbfb053efd76852dac564f764ff3deb36d6114
akilbek.ismanov <akilbek.ismanov@ANK5-YZLMORT-03>
2022-09-07 13:55:13 +0300

    Add project files.

---
+++ DIdemo/Program.cs
@@ -0,0 +1,79 @@
+﻿
+ILogger logger = new FileLogger();
+UrunService service = new UrunService(logger);
+service.Ekle();
+
+KategoriService kategoriService = new KategoriService()
+{
+    Logger = new EventLogger()
+};
+MagazaService magazaService = new MagazaService();
+magazaService.Sil(new EventLogger());
+
+
+class UrunService // urun'u  eklemek için class ve Logger üzerinden buraya log çekeriz
+{
+    private readonly ILogger _logger;
+
+    public UrunService(ILogger logger)  // constructor injection
+    {
+        _logger = logger;
+    }
+
+    public void Ekle()
+    {
+        Console.WriteLine("Ürün Eklendi");  
+        _logger.Log();
+
+    }
+}
+
+class KategoriService // property injection  
+{
+    public ILogger Logger { get; set; } // ozellikle log işlemi yapıyoruz
+    public void Guncelle()
+    {
+        Console.WriteLine("Kategori guncellendi");
+        Logger.Log();
+    }
+}
+
+class MagazaService  // method injection
+{
+    public void Sil(ILogger logger) // method'la log işleni yapıyoruz
+    {
+        Console.WriteLine("Magaza silindi");
+        logger.Log();
+    }
+}
+
+
+
+interface ILogger // log ınterface yukarıda log'lamak için 
+{
+    void Log();
+}
+
+class FileLogger : ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged to file");
+    }
+}
+
+class DatabaseLogger : ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged to database");
+    }
+}
+
+class EventLogger : ILogger  // event log kullanım ornegi
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged to Windows Event Log");
+    }
+}
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace DependencyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            SurucuBase surucu1 = new Surucu(" Efe");
+            Araba araba = new Araba(surucu1);
+            araba.Sur();
+
+            Surucu surucu2 = new Surucu(" Eyup");
+            araba = new Araba(surucu2);
+            araba.Sur();
+
+            surucu1.Dispose();
+            
+        }
+    }
+
+    class Araba        
+    {
+        private readonly SurucuBase _surucu; // has a relationship, katılımm: is a relationship
+
+        public Araba(SurucuBase surucu)  // constructor injection
+        {
+            _surucu = surucu;
+        } 
+
+        public void Sur()
+        {
+            Console.WriteLine("Araba" + _surucu.Adi + " tarafından" + HizHesapla() + " ile suruluyor...");
+        }
+
+        string HizHesapla()
+        {
+            return " 70 km/h";
+        }
+    }
+
+    abstract class SurucuBase : IDisposable
+    {
+        public string Adi { get; set; }
+        protected SurucuBase(string adi)
+        {
+            Adi = adi;
+        }
+        protected SurucuBase()  // default constructor
+        {
+            Adi = "Çagıl";
+        }
+        public void Dispose()
+        {
+            Adi = "";
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    class Surucu : SurucuBase
+    {
+        public Surucu(string adi) : base(adi)
+        {
+            Adi = adi;
+        }
+        public Surucu() : base()
+        {
+
+        }
+
+    }
+}
